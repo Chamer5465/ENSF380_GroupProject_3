bdd4c7616a3ee8f78a78961eb8a541ac6e6536de
Chamer5465 <chasemack@shaw.ca>
2025-02-26 21:19:35 -0700

    Changed Location to List Interfaces

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -1,20 +1,19 @@
 package edu.ucalgary.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Location {
     private String name;
     private String address;
-    private DisasterVictim[] occupants;
-    private Supply[] supplies;
-    private int occupantCount;
-    private int supplyCount;
+    private List<DisasterVictim> occupants;
+    private List<Supply> supplies;
 
     public Location(String name, String address) {
         this.name = name;
         this.address = address;
-        this.occupants = new DisasterVictim[100]; //Assuming a maximum of 100 occupants
-        this.supplies = new Supply[100]; //Assuming a maximum of 100 supplies
-        this.occupantCount = 0;
-        this.supplyCount = 0;
+        this.occupants = new ArrayList<DisasterVictim>();
+        this.supplies = new ArrayList<Supply>();
     }
 
     public String getName() {
@@ -33,61 +32,35 @@ public class Location {
         this.address = address;
     }
 
-    public DisasterVictim[] getOccupants() {
-        DisasterVictim[] result = new DisasterVictim[occupantCount];
-        System.arraycopy(occupants, 0, result, 0, occupantCount);
-        return result;
+    public List<DisasterVictim> getOccupants() {
+        return this.occupants;
     }
 
-    public void setOccupants(DisasterVictim[] newOccupants) {
-        this.occupants = new DisasterVictim[100];
-        this.occupantCount = newOccupants.length;
-        System.arraycopy(newOccupants, 0, this.occupants, 0, newOccupants.length);
+    public void setOccupants(List<DisasterVictim> newOccupants) {
+        this.occupants = new ArrayList<DisasterVictim>(newOccupants);
     }
 
     public void addOccupant(DisasterVictim occupant) {
-        if (occupantCount < occupants.length) {
-            occupants[occupantCount++] = occupant;
-        }
+        this.occupants.add(occupant);
     }
 
     public void removeOccupant(DisasterVictim occupant) {
-        for (int i = 0; i < occupantCount; i++) {
-            if (occupants[i].equals(occupant)) {
-                // Shift all elements after the removed occupant to the left
-                System.arraycopy(occupants, i + 1, occupants, i, occupantCount - i - 1);
-                occupantCount--;
-                break;
-            }
-        }
+        this.occupants.remove(occupant);
     }
 
-    public Supply[] getSupplies() {
-        Supply[] result = new Supply[supplyCount];
-        System.arraycopy(supplies, 0, result, 0, supplyCount);
-        return result;
+    public List<Supply> getSupplies() {
+        return this.supplies;
     }
 
-    public void setSupplies(Supply[] newSupplies) {
-        this.supplies = new Supply[100];
-        this.supplyCount = newSupplies.length;
-        System.arraycopy(newSupplies, 0, this.supplies, 0, newSupplies.length);
+    public void setSupplies(List<Supply> newSupplies) {
+        this.supplies = new ArrayList<Supply>(newSupplies);
     }
 
     public void addSupply(Supply supply) {
-        if (supplyCount < supplies.length) {
-            supplies[supplyCount++] = supply;
-        }
+        this.supplies.add(supply);
     }
 
     public void removeSupply(Supply supply) {
-        for (int i = 0; i < supplyCount; i++) {
-            if (supplies[i].equals(supply)) {
-                // Shift all elements after the removed supply to the left
-                System.arraycopy(supplies, i + 1, supplies, i, supplyCount - i - 1);
-                supplyCount--;
-                break;
-            }
-        }
+        this.supplies.remove(supply);
     }
 }
